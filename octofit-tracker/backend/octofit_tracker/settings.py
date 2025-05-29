9a98fb5c4d57bb30fb8f8970359babcd8d81b2bb
Mohammed Swaleh Nadeem <53415113+msna121@users.noreply.github.com>
2025-05-29 19:27:54 +0000

    Simplify djongo database configuration to basic settings

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -105,16 +105,9 @@ WSGI_APPLICATION = "octofit_tracker.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "djongo",
-        "NAME": "octofit_db",
-        "ENFORCE_SCHEMA": True,
-        "CLIENT": {
-            "host": "localhost",
-            "port": 27017,
-            "username": "",
-            "password": "",
-        },
+    'default': {
+        'ENGINE': 'djongo',
+        'NAME': 'octofit_db',
     }
 }
 
