c1916e981f0dae86a7867cf1b3f5a0f6207e5a00
Mohammed Swaleh Nadeem <53415113+msna121@users.noreply.github.com>
2025-05-29 19:53:53 +0000

    Ensure djongo database engine is correctly configured in settings.py

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -74,16 +74,12 @@ TEMPLATES = [
 WSGI_APPLICATION = "octofit_tracker.wsgi.application"
 
 # Database
+# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+
 DATABASES = {
     "default": {
         "ENGINE": "djongo",
         "NAME": "octofit_db",
-        "CLIENT": {
-            "host": "localhost",
-            "port": 27017,
-            "username": "",
-            "password": "",
-        },
     }
 }
 
